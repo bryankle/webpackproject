8c67a51dc8568be7d1b4c5da5aaa46c0917af0a4
Bryan <bryan@bryanle.io>
2017-07-15 09:34:36 -0400

    Set presets in .babelrc and added css loader & style loader in webpack config

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        use: ['style-loader', 'css-loader'], // Executes right to left. css-loader allows webpack to interpret css files and style-loader implements into html document
+        test: /\.css$/
+      }
+    ]
+  }
+};
